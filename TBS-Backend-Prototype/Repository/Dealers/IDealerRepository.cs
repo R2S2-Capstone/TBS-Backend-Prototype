a4a6943efc6abfdc0231b25924cab55332bd815a
reecerose <reecerose18@gmail.com>
2019-04-03 16:31:42 -0400

    Added interfaces for both repositories

---
+++ TBS-Backend-Prototype/Repository/Dealers/IDealerRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TBS_Backend_Prototype.Models;
+
+namespace TBS_Backend_Prototype.Repository.Dealers
+{
+    public interface IDealerRepository
+    {
+        IEnumerable<Dealer> GetAllDealers();
+        Dealer GetById(int id);
+        void Add(Dealer dealer);
+        void Remove(int id);
+        void Update(Dealer dealer);
+        bool DealerExists(int id);
+    }
+}
---
+++ TBS-Backend-Prototype/Repository/Vehicles/IVehicleRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using TBS_Backend_Prototype.Models;
+
+namespace TBS_Backend_Prototype.Repository.Vehicles
+{
+    public interface IVehicleRepository
+    {
+        IEnumerable<Vehicle> GetAllVehicles();
+        Vehicle GetById(int id);
+        void Add(Vehicle vehicle);
+        void Remove(int id);
+        void Update(Vehicle vehicle);
+        bool VehicleExists(int id);
+    }
+}
